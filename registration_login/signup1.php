2d6e2aa9ca10324dfb707a04bfd21822a1d79dcc
prateekpm123 <prateekpm123@gmail.com>
2019-10-28 01:28:51 +0530

    Added folders to systematize things, and included courses page and no update on singup page

---
+++ registration_login/signup1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Register</title>
+
+	<!-- Bootstrap 4 CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="signup1.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row"></div>		
+			<div class="col-md-4 offset-md-4 form-div">
+
+				<form action="registration.php" method="post">
+					<h3 class="text-center">Register</h3>
+					
+					<!-- <div class="alert alert-danger">
+						<li>Username required</li>
+					</div> -->
+
+					<div class="form-group">
+						<label>Username</label>
+						  <input type="text" name="user" class="form-control form-control lg">
+					</div>
+					<div class="form-group">
+						<label for="password">Password</label>
+						  <input type="password" name="password" class="form-control form-control lg">
+					</div>
+					<button type="submit" class="btn-btn-primary"> Login</button>
+				
+						
+					<p class="text-center">Already a member? <a href="homepage.html">Sign In</a></p>
+				</form>
+
+			</div>
+	</div>
+</body>
+</html>
